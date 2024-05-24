c7a812bd0fdd82d704078711bcd6d8ad5f4127a5
Zaki Khairi <zakielbavarian@gmail.com>
2024-05-24 22:01:47 +0700

    feat: adding delete modal with reactives, onClickOutside vueuse, and lifeCycle Hooks

--- src/components/Notes/Note.vue
+++ src/components/Notes/Note.vue
@@ -18,13 +18,18 @@
         Edit
     </RouterLink>
       <a
-        @click.prevent="storeNotes.deleteNote(note.id)"
+        @click.prevent="modals.deleteNote = true"
         href="#"
         class="card-footer-item"
       >
         Delete
       </a>
     </footer>
+    <ModalDeleteNote 
+      v-if="modals.deleteNote"
+      v-model="modals.deleteNote"
+      :noteId="note.id"
+    />
   </div>
 </template>
 
@@ -32,8 +37,9 @@
   /*
     imports
   */
-    import { computed } from 'vue';
+    import { computed, reactive } from 'vue';
     import { useStoreNotes } from '@/stores/storeNotes';
+    import ModalDeleteNote from './ModalDeleteNote.vue';
 
   /*
     props
@@ -58,4 +64,11 @@
       let description = length > 1 ? ' characters' : 'character'
       return `${length} ${description}`
     })
+
+  /*
+    modals
+  */
+  const modals = reactive({
+    deleteNote: false 
+  })  
 </script>
